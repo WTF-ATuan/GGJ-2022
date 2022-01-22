bac266d25b2003b134fdbca32c4301c8bfde5d68
Benson <jain60501@gmail.com>
2022-01-22 09:56:21 +0800

    added GameRule

---
+++ Assets/Scripts/Rule/GameRule.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Rule
+{
+    public class GameRule : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent failGameViewEvent;
+        [SerializeField] private UnityEvent passGameViewEvent;
+
+        public void FailGame()
+        {
+            failGameViewEvent?.Invoke();
+        }
+
+        public void PassGame()
+        {
+            passGameViewEvent?.Invoke();
+        }
+    }
+}
