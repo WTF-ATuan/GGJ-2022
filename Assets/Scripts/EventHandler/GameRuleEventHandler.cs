6bfc9a0d44edf35bbdfaa6c3b0a4bc1eee0e6699
Benson <jain60501@gmail.com>
2022-01-22 10:21:55 +0800

    added Event Handler

---
+++ Assets/Scripts/EventHandler/GameRuleEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Rule;
+using UnityEngine;
+
+namespace EventHandler
+{
+    [RequireComponent(typeof(GameRule))]
+    public class GameRuleEventHandler : MonoBehaviour
+    {
+        [Header("Event Poster")] [SerializeField]
+        private Actor.Actor actor;
+
+        private GameRule _gameRule;
+
+        private void Start()
+        {
+            _gameRule = GetComponent<GameRule>();
+            BindingEvent();
+        }
+
+        private void BindingEvent()
+        {
+            actor.actorDead.RegisterEvent(OnActorDead);
+        }
+
+        private void OnActorDead()
+        {
+            _gameRule.FailGame();
+        }
+    }
+}
