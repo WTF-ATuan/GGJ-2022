e8974b50abdfab08dfc8be00fdf68329dcf36bfa
CYi <cpa11225637@gmail.com>
2022-01-22 10:52:36 +0800

    Enemy script function Move.
    Enemy script class added.

--- Assets/Scripts/Magnet/AttackMagnet.cs
+++ Assets/Scripts/Magnet/AttackMagnet.cs
@@ -4,35 +4,44 @@ using UnityEngine;
 
 public class AttackMagnet : MonoBehaviour
 {
-    [SerializeField] private int enemyMagnetManagerMax;
-    private int enemyMagnetManager;
-
-    [SerializeField] private GameObject enemyMagnet;
-
-    private bool isNeedCallEnemy { get => countDown == 0f; }
-    private List<GameObject> enemyMaget_list;
+    [SerializeField] private EnemyMagnet enemyMagnet;
+    private bool isNeedCallEnemy { get => countDown == 0f;}
+    private List<EnemyMagnet> enemyMaget_list;
 
     public float countDownMax = 5f;
     private float countDown = 0f;
 
     void Start()
     {
-        enemyMaget_list = new List<GameObject>();
+        enemyMaget_list = new List<EnemyMagnet>();
 
         StartCoroutine(CallEnemy());
     }
-
     IEnumerator CallEnemy()
     {
         while (true)
         {
             yield return new WaitUntil(() => isNeedCallEnemy);
             float _randomRange_Y = Random.Range(-5f, 5f);
-            GameObject _enemyMagnet = Instantiate(enemyMagnet, transform.position + new Vector3(_randomRange_Y, 0, 0), Quaternion.identity);
+            EnemyMagnet _enemyMagnet = new EnemyMagnet();
+            _enemyMagnet.enemyMagnetObj = Instantiate(enemyMagnet.enemyMagnetPrefab, transform.position + Vector3.right * _randomRange_Y, Quaternion.identity);
+            _enemyMagnet.rigidbody2D = _enemyMagnet.enemyMagnetObj.GetComponent<Rigidbody2D>();
+            StartCoroutine(WakeUpForEnemy(_enemyMagnet));
             enemyMaget_list.Add(_enemyMagnet);
             yield return null;
         }
     }
+
+    IEnumerator WakeUpForEnemy(EnemyMagnet _enemyMagnet)
+    {
+        // Going to player
+        while (!_enemyMagnet.isBeTrigger)
+        {
+            _enemyMagnet.rigidbody2D.velocity = new Vector2(0, -1f);
+            yield return null;
+        }
+    }
+
 
     private void Update()
     {
@@ -43,5 +52,17 @@ public class AttackMagnet : MonoBehaviour
     {
         return countDownMax >= countDown ? Time.deltaTime : -countDown;
     }
+}
 
+[System.Serializable]
+public class EnemyMagnet
+{
+    public GameObject enemyMagnetPrefab;
+    [HideInInspector]public GameObject enemyMagnetObj; // In the scene
+    [HideInInspector] public Rigidbody2D rigidbody2D;
+    //被玩家用正確磁極碰到
+    public bool isBeTrigger = false; 
+
+    //當前磁極
+    public Magnet.MagneticPole m_MagneticPole;
 }
