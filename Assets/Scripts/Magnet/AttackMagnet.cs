82b4d5d787bc70515c8db23184e7d6eabefbcc96
Benson <jain60501@gmail.com>
2022-01-22 09:21:18 +0800

    新增 磁鐵folder

---
+++ Assets/Scripts/Magnet/AttackMagnet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackMagnet : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyMagnet;
+    private Vector2 boundary;
+
+    public int enemyMagnetManagerMax;
+    public int enemyMagnetManager;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
