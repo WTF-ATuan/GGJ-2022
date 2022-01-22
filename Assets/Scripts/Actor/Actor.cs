ac8fb342fbd4c56a875d93ad1e3e65e6d053a23b
Benson <jain60501@gmail.com>
2022-01-22 09:31:19 +0800

    Actor Beaten and 切換磁極

--- Assets/Scripts/Actor/Actor.cs
+++ Assets/Scripts/Actor/Actor.cs
@@ -1,18 +1,26 @@
 using System;
+using Magnet;
 using UnityEngine;
 
 namespace Actor
 {
     public class Actor : MonoBehaviour
     {
-        [SerializeField] private float defaultMoveSpeed;
+        [SerializeField] private float defaultMoveSpeed = 5;
+        [SerializeField] private MagneticPole defaultMagneticPole = MagneticPole.North;
+        [SerializeField] private int defaultHealth = 3;
+
         private float _moveSpeed;
         private Rigidbody2D _rigidbody2D;
+        private MagneticPole _currentMagneticPole;
+        private int _health;
 
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _moveSpeed = defaultMoveSpeed;
+            _currentMagneticPole = defaultMagneticPole;
+            _health = defaultHealth;
         }
 
         public void Movement(float horizontal)
@@ -27,6 +35,22 @@ namespace Actor
         {
             if(amount < 0 || amount > 100) return;
             _moveSpeed += amount;
+        }
+
+        public void SwitchMagneticPole(MagneticPole magneticPole)
+        {
+            var isSame = _currentMagneticPole == magneticPole;
+            if (isSame) return;
+            _currentMagneticPole = magneticPole;
+        }
+
+        public void Beaten()
+        {
+            _health -= 1;
+            if (_health <= 0)
+            {
+                Debug.Log("Actor Die");
+            }
         }
     }
 }
