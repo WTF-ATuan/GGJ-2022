c5546ad53b141c798992a14c474deed5285cdbb2
CYi <cpa11225637@gmail.com>
2022-01-22 09:27:23 +0800

    Change Variable public and private.
    Added scene inside Gameobject "boundary"
    Added actor(Script) to Player.

--- Assets/Scripts/Magnet/AttackMagnet.cs
+++ Assets/Scripts/Magnet/AttackMagnet.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class AttackMagnet : MonoBehaviour
 {
     [SerializeField] private GameObject enemyMagnet;
-    private Vector2 boundary;
 
-    public int enemyMagnetManagerMax;
-    public int enemyMagnetManager;
+    [SerializeField] private int enemyMagnetManagerMax;
+    private int enemyMagnetManager;
+
 
     void Start()
     {
