7310c3079ba012184f1f4a4763f6838a6cc30c28
Benson <jain60501@gmail.com>
2022-01-22 11:51:01 +0800

    Actor Collider 2D

--- Assets/Scripts/Actor/Actor.cs
+++ Assets/Scripts/Actor/Actor.cs
@@ -1,4 +1,3 @@
-using System;
 using Extra;
 using Magnet;
 using UnityEngine;
---
+++ Assets/Scripts/Actor/ActorCollider2D.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Actor
+{
+    public class ActorCollider2D : MonoBehaviour
+    {
+        private Actor _actor;
+
+        private void Start()
+        {
+            _actor = GetComponent<Actor>();
+        }
+
+        public void OnCollisionEnter2D(Collision2D other)
+        {
+
+        }
+    }
+}
