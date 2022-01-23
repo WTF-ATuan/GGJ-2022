fb692b279727a9ec4ad8fa145a57789c21bfba38
ATuan <4A8K0069@stust.edu.tw>
2022-01-23 15:57:28 +0800

    Sound Manager

---
+++ Assets/Scripts/SoundEffect/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SoundEffect{
+	public class SoundManager : MonoBehaviour{
+		public static SoundManager instance;
+		[SerializeField] private List<AudioClip> audioClips;
+		private AudioSource _audioSource;
+
+		private void Start(){
+			_audioSource = GetComponent<AudioSource>();
+			if(instance == null){
+				instance = this;
+			}
+		}
+
+		public void PlaySoundEffect(string effectName){
+			foreach(var audioClip in from audioClip in audioClips
+				let clipName = audioClip.name
+				let isSame = clipName.Equals(effectName)
+				where isSame
+				select audioClip){
+				_audioSource.PlayOneShot(audioClip);
+			}
+		}
+	}
+}
