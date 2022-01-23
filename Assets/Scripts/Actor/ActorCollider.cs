94d028483f2286ddf80105d14c4a8a8ac265b9cf
ATuan <4A8K0069@stust.edu.tw>
2022-01-23 16:03:37 +0800

    音樂音效 test good

--- Assets/Scripts/Actor/ActorCollider.cs
+++ Assets/Scripts/Actor/ActorCollider.cs
@@ -1,4 +1,5 @@
 ﻿using Magnet;
+using SoundEffect;
 using UnityEngine;
 
 // ReSharper disable All
@@ -24,6 +25,7 @@ namespace Actor
                 if (canBeatenActor)
                 {
                     _actor.GethealthObject().Open = false;
+                    SoundManager.instance.PlaySoundEffect("背包被破壞");
                 }
                 else
                 {
@@ -31,6 +33,7 @@ namespace Actor
                     _actor.Sp_C(enemy.magneticPole);
                     // 產生反擊子彈
                     Enemy_Backtrack enemy_Backtrack = Instantiate(AttackMagnet._.enemy_Backtrack, enemy.transform.position, Quaternion.identity);
+                    SoundManager.instance.PlaySoundEffect("反擊");
                     enemy_Backtrack.magneticPole = enemy.magneticPole;
                     enemy_Backtrack.End_Act += () => {
                         if (enemy.magneticPole == MagneticPole.North)
@@ -52,6 +55,7 @@ namespace Actor
             AddHP addHp = other.GetComponent<AddHP>();
             if (addHp != null)
             {
+                SoundManager.instance.PlaySoundEffect("撿起卡爾");
                 if (_actor._currentMagneticPole != addHp.magneticPole)
                 {
                     HealthObject healthObject = _actor.GethealthObject(false);
