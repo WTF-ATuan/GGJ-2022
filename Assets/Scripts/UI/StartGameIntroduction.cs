8a6f14e744fe6c30a4420ec9f804f177552a47c6
ja3527123 <ja3527123@gmail.com>
2022-01-23 16:54:23 +0800

    遊戲說明的雞掰人旁邊兩個磁鐵會浮動

--- Assets/Scripts/UI/StartGameIntroduction.cs
+++ Assets/Scripts/UI/StartGameIntroduction.cs
@@ -11,10 +11,13 @@ public class StartGameIntroduction : MonoBehaviour
 
     //Boss image change
     public Image bossImage;
+    public Image RedStom;
+    public Image BuleStom;
     public Sprite boss01Sprite, boss02Sprite, boss03Sprite;
     public float minChangeTime = 0.1f, maxChangeTime = 3f;
     public float shakePower = 5f;
-    void Start()
+
+    private void OnEnable()
     {
         StartTextUI_Pots = new Vector2[TextUIs.Length];
         for (int i = 0; i < TextUIs.Length; i++)
@@ -43,6 +46,26 @@ public class StartGameIntroduction : MonoBehaviour
             {
                 yield return bossImage.transform.DOLocalMoveY(startv2.y + 50, 5f).WaitForCompletion();
                 yield return bossImage.transform.DOLocalMoveY(startv2.y, 5f).WaitForCompletion();
+            }
+        }
+        StartCoroutine(IE_RedStomFly());
+        IEnumerator IE_RedStomFly()
+        {
+            Vector2 startv2 = bossImage.transform.localPosition;
+            for (; ; )
+            {
+                yield return RedStom.transform.DOLocalMoveY(startv2.y + 40, 4f).WaitForCompletion();
+                yield return RedStom.transform.DOLocalMoveY(startv2.y - 40, 4f).WaitForCompletion();
+            }
+        }
+        StartCoroutine(IE_BlueStomFly());
+        IEnumerator IE_BlueStomFly()
+        {
+            Vector2 startv2 = bossImage.transform.localPosition;
+            for (; ; )
+            {
+                yield return BuleStom.transform.DOLocalMoveY(startv2.y - 40, 4f).WaitForCompletion();
+                yield return BuleStom.transform.DOLocalMoveY(startv2.y + 40, 4f).WaitForCompletion();
             }
         }
     }
